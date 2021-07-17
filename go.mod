0726c2b5df40dbd88d20af4114eca1aa5deb5052
holyshared <holy.shared.design@gmail.com>
2021-07-17 16:26:11 +0900

    add twilio-go

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module terraform-provider-twilio
 
 go 1.15
 
-require github.com/hashicorp/terraform-plugin-sdk/v2 v2.7.0
+require (
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.7.0
+	github.com/twilio/twilio-go v0.12.0
+)
--- twilio/provider.go
+++ twilio/provider.go
@@ -1,14 +1,49 @@
 package twilio
 
 import (
+	"context"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/twilio/twilio-go"
 )
+
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	accountSid := d.Get("accountSid").(string)
+	authToken := d.Get("authToken").(string)
+
+	var diags diag.Diagnostics
+
+	if (accountSid == "") || (authToken == "") {
+		return nil, diag.FromErr(fmt.Errorf("Error: %s", "accountSid and authToken is required"))
+	}
+
+	client := twilio.NewRestClientWithParams(twilio.RestClientParams{
+		Username: accountSid,
+		Password: authToken,
+	})
+
+	return client, diags
+}
 
 func Provider() *schema.Provider {
 	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"accountSid": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("TWILIO_ACCOUNT_SID", nil),
+			},
+			"authToken": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Sensitive:   true,
+				DefaultFunc: schema.EnvDefaultFunc("TWILIO_AUTH_TOKEN", nil),
+			},
+		},
 		ResourcesMap: map[string]*schema.Resource{},
 		DataSourcesMap: map[string]*schema.Resource{
 			"twilio_chat_services": dataSourceChatServices(),
 		},
+		ConfigureContextFunc: providerConfigure,
 	}
 }
