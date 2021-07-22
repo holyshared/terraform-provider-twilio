d7ffd60063bfa8b81f0398c770f64eca59b1f035
holyshared <holy.shared.design@gmail.com>
2021-07-22 12:56:19 +0900

    fix examples

--- examples/main.tf
+++ examples/main.tf
@@ -15,3 +15,30 @@ provider "twilio" {
 resource "twilio_chat_service" "terraform_dev" {
   friendly_name = "terraform-dev-1"
 }
+
+resource "twilio_chat_service" "terraform_test" {
+  friendly_name = "terraform-test-1"
+  roles {
+    default_service_role = "RL69a3adbc16d9489093fe084f4c798e30"
+    default_channel_role = "RL52ecdd95b762485db2363ccff1b82a44"
+    default_channel_creator_role = "RL5c67ab91b0b94a2cb95e5c53a263f1d1"
+  }
+  limits {
+    user_channels = 250
+    channel_members = 100
+  }
+  additional_settings {
+    reachability_enabled = true
+    read_status_enabled = true
+    consumption_report_interval = 10
+    typing_indicator_timeout = 5
+    pre_webhook_retry_count = 1
+    post_webhook_retry_count = 1
+  }
+  webhooks {
+    events = ["onMessageSend", "onMessageSent"]
+    method = "POST"
+    pre_hook_url = "https://example.com"
+    post_hook_url = "https://example.com"
+  }
+}
