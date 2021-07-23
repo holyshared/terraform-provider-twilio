45fa5e06acd2cbb61faacb2473f1e50cc06d5e81
holyshared <holy.shared.design@gmail.com>
2021-07-23 17:19:45 +0900

    add scheme validation

--- twilio/chat/resource/service/read_context.go
+++ twilio/chat/resource/service/read_context.go
@@ -5,12 +5,98 @@ import (
 	"time"
 
 	tw "github.com/twilio/twilio-go"
+	openapi "github.com/twilio/twilio-go/rest/chat/v2"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 var pushNotificationTemplateNames = []string{"new_message", "invited_to_channel", "added_to_channel", "removed_from_channel"}
+
+func rolesFromResponse(cs *openapi.ChatV2Service) *map[string]interface{} {
+	setting := map[string]interface{}{}
+
+	if cs.DefaultServiceRoleSid != nil {
+		setting["default_service_role"] = *cs.DefaultServiceRoleSid
+	}
+	if cs.DefaultChannelRoleSid != nil {
+		setting["default_channel_role"] = *cs.DefaultChannelRoleSid
+	}
+	if cs.DefaultChannelCreatorRoleSid != nil {
+		setting["default_channel_creator_role"] = *cs.DefaultChannelCreatorRoleSid
+	}
+
+	if len(setting) <= 0 {
+		return nil
+	}
+
+	return &setting
+}
+
+func limitsFromResponse(limits map[string]interface{}) map[string]interface{} {
+	setting := map[string]interface{}{}
+
+	if v, ok := limits["channel_members"].(int); ok {
+		setting["channel_members"] = v
+	}
+	if v, ok := limits["user_channels"].(int); ok {
+		setting["user_channels"] = v
+	}
+
+	return setting
+}
+
+func additionalSettingsFromResponse(cs *openapi.ChatV2Service) *map[string]interface{} {
+	setting := map[string]interface{}{}
+
+	if cs.ReachabilityEnabled != nil {
+		setting["reachability_enabled"] = *cs.ReachabilityEnabled
+	}
+	if cs.ReadStatusEnabled != nil {
+		setting["read_status_enabled"] = *cs.ReadStatusEnabled
+	}
+	if cs.ConsumptionReportInterval != nil {
+		setting["consumption_report_interval"] = *cs.ConsumptionReportInterval
+	}
+	if cs.TypingIndicatorTimeout != nil {
+		setting["typing_indicator_timeout"] = *cs.TypingIndicatorTimeout
+	}
+	if cs.PreWebhookRetryCount != nil {
+		setting["pre_webhook_retry_count"] = *cs.PreWebhookRetryCount
+	}
+	if cs.PostWebhookRetryCount != nil {
+		setting["post_webhook_retry_count"] = *cs.PostWebhookRetryCount
+	}
+
+	if len(setting) <= 0 {
+		return nil
+	}
+
+	return &setting
+}
+
+func webhookFromResponse(cs *openapi.ChatV2Service) *map[string]interface{} {
+	webhook := map[string]interface{}{}
+
+	if cs.WebhookFilters != nil {
+		webhook["events"] = *cs.WebhookFilters
+	}
+	if cs.WebhookMethod != nil {
+		webhook["method"] = *cs.WebhookMethod
+	}
+	if cs.PreWebhookUrl != nil {
+		webhook["pre_hook_url"] = *cs.PreWebhookUrl
+	}
+	if cs.PostWebhookUrl != nil {
+		webhook["post_hook_url"] = *cs.PostWebhookUrl
+	}
+
+	if len(webhook) <= 0 {
+		return nil
+	}
+
+	return &webhook
+}
 
 func templateFromResponse(template map[string]interface{}) map[string]interface{} {
 	setting := map[string]interface{}{}
@@ -69,60 +155,43 @@ func ReadContext(ctx context.Context, d *schema.ResourceData, m interface{}) dia
 		return diag.FromErr(err)
 	}
 
-	roles := []map[string]interface{}{{
-		"default_service_role":         *res.DefaultServiceRoleSid,
-		"default_channel_role":         *res.DefaultChannelRoleSid,
-		"default_channel_creator_role": *res.DefaultChannelCreatorRoleSid,
-	}}
-
+	roles := []map[string]interface{}{}
+	if ss := rolesFromResponse(res); ss != nil {
+		roles = append(roles, *ss)
+	}
 	if err := d.Set("roles", roles); err != nil {
 		return diag.FromErr(err)
 	}
 
-	limits := []map[string]interface{}{{
-		"channel_members": (*res.Limits)["channel_members"],
-		"user_channels":   (*res.Limits)["user_channels"],
-	}}
-
+	limits := []map[string]interface{}{}
+	if res.Limits != nil {
+		limits = append(limits, limitsFromResponse(*res.Limits))
+	}
 	if err := d.Set("limits", limits); err != nil {
 		return diag.FromErr(err)
 	}
 
-	settigns := []map[string]interface{}{{
-		"reachability_enabled":        *res.ReachabilityEnabled,
-		"read_status_enabled":         *res.ReadStatusEnabled,
-		"consumption_report_interval": *res.ConsumptionReportInterval,
-		"typing_indicator_timeout":    *res.TypingIndicatorTimeout,
-		"pre_webhook_retry_count":     *res.PreWebhookRetryCount,
-		"post_webhook_retry_count":    *res.PostWebhookRetryCount,
-	}}
-
-	if err := d.Set("additional_settings", settigns); err != nil {
+	additionalSettings := []map[string]interface{}{}
+	if ss := additionalSettingsFromResponse(res); ss != nil {
+		additionalSettings = append(additionalSettings, *ss)
+	}
+	if err := d.Set("additional_settings", additionalSettings); err != nil {
 		return diag.FromErr(err)
 	}
 
-	webhook := map[string]interface{}{}
-	if res.WebhookFilters != nil {
-		webhook["events"] = *res.WebhookFilters
+	webhooks := []map[string]interface{}{}
+	if ss := webhookFromResponse(res); ss != nil {
+		webhooks = append(webhooks, *ss)
 	}
-	if res.WebhookMethod != nil {
-		webhook["method"] = *res.WebhookMethod
-	}
-	if res.PreWebhookUrl != nil {
-		webhook["pre_hook_url"] = *res.PreWebhookUrl
-	}
-	if res.PostWebhookUrl != nil {
-		webhook["post_hook_url"] = *res.PostWebhookUrl
-	}
-
-	webhooks := []map[string]interface{}{webhook}
 	if err := d.Set("webhooks", webhooks); err != nil {
 		return diag.FromErr(err)
 	}
 
-	notifications := []map[string]interface{}{
-		notificationsFromResponse(*res.Notifications),
+	notifications := []map[string]interface{}{}
+	if res.Notifications != nil {
+		notifications = append(notifications, notificationsFromResponse(*res.Notifications))
 	}
+
 	if err := d.Set("notifications", notifications); err != nil {
 		return diag.FromErr(err)
 	}
